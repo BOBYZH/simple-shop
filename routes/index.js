0e5eec9c506a1a77c2d6f4bf5a2f44e18d484649
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-05-07 23:44:29 +0800

    feat: :truck: Remove unused user, and Add routes and controllers to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const memberController = require('../controllers/memberController');
+const productController = require('../controllers/productController');
+
+router.get('/', productController.getProducts);
+
+router.get('/signup', memberController.signUpPage);
+
+router.post('/signup', memberController.signUp);
+
+router.get('/signin', memberController.signInPage);
+
+router.post('/signin', memberController.signIn);
+
+router.post('/logout', memberController.logout);
 
 module.exports = router;
