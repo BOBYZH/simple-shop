9d78346ff70d3e9de4bfdec4d77ea71c3d0fdfd1
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-05-07 23:37:42 +0800

    feat: :card_file_box: Add config and template file for mariaDB

---
+++ config/mariaDB.js
@@ -0,0 +1,15 @@
+/* 選用與MySQL類似但較無授權問題的MariaDB練習 */
+function mariaDBSettings () {
+  const mariaDB = require('mariaDB');
+  const pool = mariaDB.createPool({
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD
+  });
+
+  return pool;
+}
+
+module.exports = mariaDBSettings;
