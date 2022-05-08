f3f76842fbd1177d2d49f5aef31c9e8939fa6ada
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-05-09 00:00:58 +0800

    feat: :truck: Add annotations and getProduct route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const router = express.Router();
 const memberController = require('../controllers/memberController');
 const productController = require('../controllers/productController');
 
+// 所有商品頁面(首頁)
 router.get('/', productController.getProducts);
+
+// 個別商品頁面
+router.get('/products/:id', productController.getProduct);
 
 router.get('/signup', memberController.signUpPage);
 
