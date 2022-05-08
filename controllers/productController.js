02e8813309e01b5d22ec359d0961b14b95a55a4c
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-05-09 00:04:29 +0800

    feat: :sparkles: Add getProduct route, and Update getProducts route

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,43 @@
+const mariaDBConfig = require('../config/mariaDB.js');
+const SQL = require('sql-template-strings');
+const pool = mariaDBConfig();
+
 const productController = {
-  getProducts: (req, res) => {
-    if (req.session.userName) {
-      // 有登入時顯示會員名稱
-      res.render('index', { userName: req.session.userName });
-    } else {
-      res.redirect('/signin');
+  getProducts: async (req, res) => {
+    const conn = await pool.getConnection();
+    try {
+      const products = await conn.query('SELECT * FROM product;');
+      res.render('index', { products });
+    } catch (err) {
+      await req.flash('error_messages', '系統錯誤！');
+      res.redirect('back');
+      throw err;
+    } finally {
+      if (conn) conn.release();
+    }
+  },
+
+  getProduct: async (req, res) => {
+    const conn = await pool.getConnection();
+    try {
+      const showProducts = await conn.query(
+        SQL`SELECT * FROM product WHERE id = ${req.params.id};`
+      );
+      const product = showProducts[0]; // 單項去陣列，以便後續操作與EJS渲染
+
+      // 避免無庫存仍可進入下單
+      if (product.quantity === 0) {
+        await req.flash('error_messages', '產品已經售完！');
+        return res.redirect('back');
+      } else {
+        res.render('product', { product });
+      }
+    } catch (err) {
+      await req.flash('error_messages', '系統錯誤！');
+      res.redirect('back');
+      throw err;
+    } finally {
+      if (conn) conn.release();
     }
   }
 };
