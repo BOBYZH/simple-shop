f232d7dc05e986535d11582e27f7ee91f00f53d5
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-05-10 00:48:55 +0800

    feat: :truck: Add cartController, router.all for 404, and more annotations,

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,36 @@ const router = express.Router();
 
 const memberController = require('../controllers/memberController');
 const productController = require('../controllers/productController');
+const cartController = require('../controllers/cartController');
 
+// 商品
 // 所有商品頁面(首頁)
 router.get('/', productController.getProducts);
-
 // 個別商品頁面
 router.get('/products/:id', productController.getProduct);
 
+// 購物車
+// 購物車內容
+router.get('/cart', cartController.getCart);
+// 新增商品
+router.post('/cart', cartController.postCart);
+
+// 帳戶
+// 註冊頁面
 router.get('/signup', memberController.signUpPage);
-
+// 註冊
 router.post('/signup', memberController.signUp);
-
+// 登入頁面
 router.get('/signin', memberController.signInPage);
-
+// 登入
 router.post('/signin', memberController.signIn);
-
+// 登出
 router.post('/logout', memberController.logout);
+
+// 避免404畫面出現
+router.all('*', async (req, res) => {
+  await req.flash('errorMessages', '無效的連結！');
+  res.redirect('/');
+});
 
 module.exports = router;
