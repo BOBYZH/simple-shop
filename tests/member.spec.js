2a5b0827ce9cf046389ab177a39226bff194a2e8
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-06-14 00:50:44 +0800

    test: :white_check_mark: Add test file for memberController

---
+++ tests/member.spec.js
@@ -0,0 +1,108 @@
+/* eslint-env mocha */
+// const { expect } = require('chai');
+const { expect } = require('chai');
+const chai = require('chai');
+chai.use(require('sinon-chai'));
+chai.should(); // 包含include語法
+const request = require('supertest');
+const app = require('../app');
+
+const mariaDBConfig = require('../config/mariaDB.js');
+const pool = mariaDBConfig();
+let conn;
+
+describe('# 會員系統', () => {
+  before(async () => {
+    conn = await pool.getConnection();
+    await conn.query('SET FOREIGN_KEY_CHECKS = 0;');
+    await conn.query('TRUNCATE TABLE `member`;');
+    await conn.query('SET FOREIGN_KEY_CHECKS = 1;');
+  });
+
+  context('## GET /signup', () => {
+    it('### 進入註冊頁面', (done) => {
+      request(app)
+        .get('/signup')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          res.text.should.include('註冊');
+          return done();
+        });
+    });
+  });
+
+  context('## POST /signup', () => {
+    it('### 成功註冊', (done) => {
+      request(app)
+        .post('/signup')
+        .send({
+          email: 'user@email.com',
+          password: 'password',
+          password2: 'password'
+        })
+        .set('Accept', 'application/json')
+        .expect(302)
+        .end(async (err, res) => {
+          if (err) return done(err);
+          const member = await conn.query('SELECT email FROM member;');
+          expect(member[0].email).to.equal('user@email.com');
+          expect(res.header.location).to.equal('/signin');
+          return done(err);
+        });
+    });
+  });
+
+  context('## GET /signin', () => {
+    it('### 進入登入頁面', (done) => {
+      request(app)
+        .get('/signin')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          res.text.should.include('登入');
+          return done();
+        });
+    });
+  });
+
+  context('## POST /signin', () => {
+    it('### 成功登入', (done) => {
+      request(app)
+        .post('/signin')
+        .send({
+          email: 'user@email.com',
+          password: 'password',
+          password2: 'password'
+        })
+        .set('Accept', 'application/json')
+        .expect(302)
+        .end(async (err, res) => {
+          if (err) return done(err);
+          expect(res.header.location).to.equal('/');
+          return done(err);
+        });
+    });
+  });
+
+  context('## POST /logout', () => {
+    it('### 成功登出', (done) => {
+      request(app)
+        .post('/logout')
+        .set('Accept', 'application/json')
+        .expect(302)
+        .end(async (err, res) => {
+          if (err) return done(err);
+          expect(res.header.location).to.equal('/');
+          return done(err);
+        });
+    });
+  });
+
+  after(async () => {
+    await conn.query('SET FOREIGN_KEY_CHECKS = 0;');
+    await conn.query('TRUNCATE TABLE `member`;');
+    await conn.query('SET FOREIGN_KEY_CHECKS = 1;');
+    await conn.release();
+  });
+});
