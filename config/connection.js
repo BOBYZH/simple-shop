1971f77de67890dff3189c34a5fbc00338298464
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-07-02 02:02:51 +0800

    fix: :ambulance: Use lowercase in module name

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 async function connection (pool) {
-  const mariaDB = require('mariaDB');
+  // 套件名稱都是小寫，DB不要用大寫，否則測試(Github Actions)和部署環境(heroku)會找不到套件
+  const mariaDB = require('mariadb');
   if (process.env.NODE_ENV !== 'production') {
     return await pool.getConnection();
   } else { // 開發環境使用JAWSDB MARIA
