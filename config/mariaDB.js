812a387d3e0e0551d29831c9177a44a249be9f89
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2022-07-02 01:58:31 +0800

    fix: :ambulance: Use lowercase in module name

--- config/mariaDB.js
+++ config/mariaDB.js
@@ -1,6 +1,7 @@
 /* 選用與MySQL類似但較無授權問題的MariaDB練習 */
 function mariaDBSettings () {
-  const mariaDB = require('mariaDB');
+  // 套件名稱都是小寫，DB不要用大寫，否則測試(Github Actions)和部署環境(heroku)會找不到套件
+  const mariaDB = require('mariadb');
   const pool = mariaDB.createPool({
     database: process.env.DB, // 使用的資料庫名稱(如本專案seeder產生的"simple_shop")
     host: process.env.DB_HOST, // 資料庫的URL或ip位址(本機預設為127.0.0.1)
